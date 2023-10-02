b0b154f07c0e3ef2763058a7c6efb341eb7bc6f1
RAKHYUN <kyle2598@gmail.com>
2023-10-02 14:39:08 -0500

    - change friend to staranger

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, wodsdfrld!")
+            Text("Hello, stranger!")
         }
         .padding()
     }
