ae9b33ded17a902a8fcf54f3d6d41498d114ad92
RAKHYUN <kyle2598@gmail.com>
2023-10-02 14:30:17 -0500

    Initial Commit

---
+++ CitySights App/CitySights_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySights_AppApp.swift
+//  CitySights App
+//
+//  Created by Rakhyun Kim on 10/2/23.
+//
+
+import SwiftUI
+
+@main
+struct CitySights_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySights App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CitySights App
+//
+//  Created by Rakhyun Kim on 10/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
