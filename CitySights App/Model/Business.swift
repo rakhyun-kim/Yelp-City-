6d3c3346e0426a5ec138661a1f42d981d08a3214
RAKHYUN <kyle2598@gmail.com>
2023-10-03 14:22:05 -0500

    - Add codingkeys to business struct and location struct

--- CitySights App/Model/Business.swift
+++ CitySights App/Model/Business.swift
@@ -12,17 +12,38 @@ struct Business: Decodable, Identifiable {
     var alias: String?
     var categories: [Category]
     var coordinates: Coordinate?
-    var diplay_phone: String?
+    var displayPhone: String?
     var distance: Double?
-    var image_url: String?
-    var is_close: Bool?
+    var imageUrl: String?
+    var isClose: Bool?
     var location: Location?
     var name: String?
     var phone: String?
     var price: String?
     var rating: Double?
-    var review_count: Int?
+    var reviewCount: Int?
     var url: String?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case displayPhone = "display_phone"
+        case isClose = "is_close"
+        case imageUrl = "image_url"
+        case reviewCount = "review_count"
+        
+        case id
+        case alias
+        case categories
+        case coordinates
+        case distance
+        case location
+        case name
+        case phone
+        case price
+        case rating
+        case url
+        
+    }
 }
 
 struct Category: Decodable {
@@ -36,7 +57,20 @@ struct Location: Decodable {
     var address3: String?
     var city: String?
     var county: String?
-    var display_address: [String]?
+    var displayAddress: [String]?
     var state: String?
-    var zip_code: String?
+    var zipCode: String?
+    
+    enum CodingKeys: String, CodingKey {
+        
+        case zipCode = "zip_code"
+        case displayAddress = "isplay_address"
+        
+        case address1
+        case address2
+        case address3
+        case city
+        case county
+        case state
+    }
 }
