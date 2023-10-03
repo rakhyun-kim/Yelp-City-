0f08f389b6317bea6fbf2fe4a8dfcc739045c9e5
RAKHYUN <kyle2598@gmail.com>
2023-10-03 10:46:00 -0500

    - add config file - add gitignore - add api key

--- CitySights App/ContentView.swift
+++ CitySights App/ContentView.swift
@@ -8,19 +8,27 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var query = ""
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, stranger!")
-            
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, stranger!")
+        HStack {
+            TextField("What are you looking for", text: $query)
+            Button(action: {
+                
+            }, label: {
+                Text("Go")
+                    .padding()
+                    .background(.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    
+            })
         }
         .padding()
+        .onAppear(perform: {
+            print(Bundle.main.infoDictionary?["API_KEY"] as? String)
+        })
     }
 }
 
